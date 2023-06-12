5caca45ead44c987424ae37debbd59c793fd6791
Micah Njeru <micahnjeru1@gmail.com>
2023-06-12 11:15:48 +0300

    Initial commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Micah Jesse Njeru on 12/06/2023.
+//
+
+import Foundation
+
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "What's the largest coastal city of Kenya?", answer: "Mombasa")
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Flashzilla iOS 16
+## About this app
+- User's will be shown a car with a prompt which teaches them about something
+- It's almost like real-life flash cards used to prepare for examinations
+
+## What I learned
+How to:
+1. Design an app that supports landscape orientation only 
+
